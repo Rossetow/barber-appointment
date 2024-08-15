21d3938f2f6c69532a18461ea8f2656bd8992db8
Rossetow <rafaelrosseto05@gmail.com>
2024-08-15 20:54:01 -0300

    Initializing project with Spring Boot and React templates

---
+++ backend/src/test/java/br/com/barberappoitment/barber_appointment/BarberAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barberappoitment.barber_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
